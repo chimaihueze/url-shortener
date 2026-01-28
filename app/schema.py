82eb6bd303ba64fafe291ba943ae51e02786dae9
chimaihueze <kihueze@gmail.com>
2026-01-28 19:06:49 +0000

    updated schema

--- app/schema.py
+++ app/schema.py
@@ -1,14 +1,24 @@
+from typing import Generic, TypeVar
 from pydantic import BaseModel, Field, AnyUrl
 from datetime import datetime
 
 
-class Request(BaseModel):
+class RequestDTO(BaseModel):
     url: AnyUrl
 
 
-class Response(BaseModel):
+class ResponseDTO(BaseModel):
     id: str = Field(max_length=36)
     url: AnyUrl
     short_code: str = Field(min_length=4, max_length=10)
     created_at: datetime
     updated_at: datetime
+
+T = TypeVar("T")
+
+class SuccessResponse(BaseModel, Generic[T]):
+    message: str
+    data: T
+
+class SuccessMessage(BaseModel):
+    message: str
